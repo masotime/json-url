c2379131b2c246275d8ac1354743bd846040a747
Jay Zawrotny <jayzawrotny@gmail.com>
2015-12-12 17:00:37 -0500

    Tested tests on 4.2.3 using `npm test`

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let expect = require('expect'),
-    jsonurl = require('.'),
+var expect = require('expect'),
+    jsonurl = require('./'),
+    Promise = require('bluebird'),
     DATA = {};
 
 DATA.json = {
@@ -23,12 +24,12 @@ DATA.json = {
   "search_for_name": "Name Search Test"
 };
 
-describe('json-url', () => {
-  describe('#compress()', () => {
-    it('Should produce a shorter string', () => {
+describe('json-url', function () {
+  describe('#compress()', function () {
+    it('Should produce a shorter string', function () {
       let lengthOfRawJSON = JSON.stringify(DATA.json).length;
 
-      jsonurl.compress(DATA.json, (err, result) => {
+      jsonurl.compress(DATA.json, function (err, result) {
         if (err) throw err;
         expect(result.length < lengthOfRawJSON).toBe(true);
         expect(result).toBe('XQAAAALbAAAAAAAAAABCKcjGk5HEXfZ_irAzWI5smk4U224-XbP8lQ6YYCU5p-w2SkyQtlmfT0VvnCdsfHm2H3--vg78vBOgRpDySu7Zzo8mJRPoQz_ZpMcCzPuxBy37YAs8Nfsx0PzXOkXo4qv3JMVgpdA04vBd0e5Cmn16Un25590ay51jHGD3vQZCciO7wPuv_ofXSFZVhhnkZoj8AY87eMhQ5tautP2CHJ_HvXhJ0KaLLyllpTmipLE3wAtoD8Bk3HeOd_35AkA');
@@ -37,11 +38,11 @@ describe('json-url', () => {
     
   });
 
-  describe('#decompress()', () => {
-    it('Should reproduce the original input', () => {
+  describe('#decompress()', function () {
+    it('Should reproduce the original input', function ()  {
       function decompress (compressedStr) {
-        return new Promise((resolve, reject) => {
-          jsonurl.decompress(compressedStr, (err, result) => {
+        return new Promise(function (resolve, reject) {
+          jsonurl.decompress(compressedStr, function (err, result) {
             if (err) return reject(err);
 
             resolve(result);
@@ -50,20 +51,22 @@ describe('json-url', () => {
       }
 
       function compress (data) {
-        return new Promise((resolve, reject) => {
-          jsonurl.compress(DATA.json, (err, result) => {
+        return new Promise( function (resolve, reject) {
+          jsonurl.compress(DATA.json, function (err, result) {
             if (err) return reject(err);
             resolve(result);
           });
         });
       }
 
+      function testDecompressedObj (jsonObj) {
+        expect(jsonObj).toEqual(DATA.json);
+      }
+
       compress(DATA.json)
       .then(decompress)
-      .then((jsonObj) => {
-        expect(jsonObj).toEqual(DATA.json);
-      })
-      .catch((err) => {
+      .then(testDecompressedObj)
+      .catch(function (err) {
         console.error(err);
       });
     });
